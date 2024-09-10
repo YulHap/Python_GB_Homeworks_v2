f40dd11c6ea05eb1849c3914517b868d8d4a20b3
YuliYa <yuh0121@mail.ru>
2024-09-10 18:41:31 +0700

    add correct seminar6:task4

--- python_seminar_6_hw/python_sem6_task4.py
+++ python_seminar_6_hw/python_sem6_task4.py
@@ -4,3 +4,29 @@
 # Напишите код, который раскрывает все вложенные списки, то есть оставляет
 # лишь внешний список. Для решения используйте только list comprehensions.
 # Ответ: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+
+# Решение 1 - list comprehensions
+
+nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
+
+list_res = [el 
+            for el_list in nice_list 
+            for el_list1 in el_list 
+            for el in el_list1]
+
+print(list_res)
+
+# Решение 2 - рекурсия без list comprehensions
+
+nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
+
+def output_list(list1:list) -> list:
+    list_res = []
+    for i in list1:
+        if type(i) is int:
+            list_res.append(i)
+        elif type(i) is list:
+            list_res.extend(output_list(i))
+    return list_res
+
+print(output_list(nice_list))
