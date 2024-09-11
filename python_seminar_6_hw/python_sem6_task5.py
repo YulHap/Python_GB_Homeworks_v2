b40c9c7990a3f10214c49476fdbb1e72af7a05df
YuliYa <yuh0121@mail.ru>
2024-09-12 00:48:16 +0700

    Add seminar6:task5

--- python_seminar_6_hw/python_sem6_task5.py
+++ python_seminar_6_hw/python_sem6_task5.py
@@ -5,3 +5,45 @@
 # буква А станет буквой Г, Б станет Д и так далее.
 # Пользователь вводит сообщение и значение сдвига. Напишите программу,
 # которая изменит фразу при помощи шифра Цезаря.
+# Пример:
+# Введите сообщение: это питон.
+# Введите сдвиг: 3
+# Зашифрованное сообщение: ахс тлхср.
+
+# Решение 1 - без использования list comprehensions
+
+txt = input('Введите сообщение: ')
+k = int(input('Введите значение сдвига: '))
+
+def сaesar_cipher(txt:str, shift:int) -> str:
+    alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    text_result = ''
+    for symbol in txt:
+        if symbol in alphabet:
+            text_result = text_result + alphabet[(alphabet.index(symbol) + k) % len(alphabet)]
+        else:
+            text_result = text_result + symbol
+    return text_result
+
+print(сaesar_cipher(txt, k))
+
+# Решение 2 - list comprehensions
+
+txt = input('Введите сообщение: ')
+k = int(input('Введите значение сдвига: '))
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+text_result = [alphabet[(alphabet.index(symbol) + k) % len(alphabet)] if symbol in alphabet else symbol for symbol in txt]
+
+print(*text_result, sep ='')
+
+
+# Решение 2.1 - list comprehensions. Решение 2 с методом join() в выводе
+
+txt = input('Введите сообщение: ')
+k = int(input('Введите значение сдвига: '))
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+text_result = [alphabet[(alphabet.index(symbol) + k) % len(alphabet)] if symbol in alphabet else symbol for symbol in txt]
+
+print(''.join(text_result))
