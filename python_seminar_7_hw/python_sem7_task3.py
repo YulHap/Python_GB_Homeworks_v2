581506780a4ab0d6a786342434b826e4c9fe956d
YuliYa <yuh0121@mail.ru>
2024-09-12 18:42:25 +0700

    Add seminar7:task3

--- python_seminar_7_hw/python_sem7_task3.py
+++ python_seminar_7_hw/python_sem7_task3.py
@@ -7,3 +7,45 @@
 # Результат:
 # True
 # False
+
+
+# Решение 1 - без collections
+
+str1 = 'abcba'
+str2 = 'abbbc'
+
+def is_palindrom(string:str):
+    i = 0
+    j = len(string) - 1
+    counter = 0
+    iter = 0
+    while i < j:
+        iter += 1
+        if string[i] == string[j]:
+            counter += 1
+        i += 1
+        j -= 1
+    if counter == int(len(string) / 2):
+        return True
+    else:
+        return False
+
+print(is_palindrom(str1))
+print(is_palindrom(str2))
+
+# Решение 2 - c collections
+
+from collections import Counter
+
+
+str1 = 'abcba'
+str2 = 'abbbc'
+
+def can_be_poly(string:str) -> bool:
+    count = Counter(string)
+    odd_symbols = len(list(filter(lambda x: x % 2 == 1, count.values())))
+    return odd_symbols < 2
+
+print(can_be_poly(str1))
+print(can_be_poly(str2))
+
