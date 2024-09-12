6362bb8e2cc75c8b90f317eb2f4894abedc082ef
YuliYa <yuh0121@mail.ru>
2024-09-12 17:13:56 +0700

    Add seminar7:task1

--- python_seminar_7_hw/python_sem7_task1.py
+++ python_seminar_7_hw/python_sem7_task1.py
@@ -8,3 +8,15 @@
 # до трёх знаков после запятой.
 # 2. Из списка names берутся только имена минимум из пяти букв.
 # 3. Из списка numbers берётся произведение всех чисел.
+
+import functools
+
+floats = [12.3554, 4.02, 5.777, 2.12, 3.13, 4.44, 11.0001]
+names = ["Vanes", "Alen", "Jana", "William", "Richards", "Joy"]
+numbers = [22, 33, 10, 6894, 11, 2, 1]
+
+floats_res1 = list(map(lambda x: round((x ** 3), 3), floats))
+names_res2 = list(filter(lambda x: len(x) >= 5, names))
+numbers_res3 = int(functools.reduce(lambda x, y: x*y, numbers))
+
+print(floats_res1, names_res2, numbers_res3, sep = '\n')
