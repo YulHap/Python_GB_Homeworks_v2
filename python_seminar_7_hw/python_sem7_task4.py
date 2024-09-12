a100c6b6aa39cef86e5b75f67185c3b72cfa5d28
YuliYa <yuh0121@mail.ru>
2024-09-13 00:52:23 +0700

    Add seminar7:task4

--- python_seminar_7_hw/python_sem7_task4.py
+++ python_seminar_7_hw/python_sem7_task4.py
@@ -10,3 +10,50 @@
 # unique_count = count_unique_characters(message)
 # print("Количество уникальных символов в строке:", unique_count)
 # Вывод: количество уникальных символов в строке — 5.
+
+
+
+# Решение 1
+
+from collections import Counter
+
+message = "Today is a beautiful day! The sun is shining and the birds are singing."
+
+def count_unique_characters(message:str) -> int:
+    count_unic = list(filter(lambda v: v == 1, Counter(message.lower()).values()))
+    return len(count_unic)
+
+print(f'Количество уникальных символов в строке: {count_unique_characters(message)}')
+
+
+
+# Решение 2 - без lambda-функций map, filter
+
+from collections import Counter
+
+message = "Today is a beautiful day! The sun is shining and the birds are singing."
+
+def count_unique_characters(message:str) -> int:
+    count_symbol = Counter(message.lower())
+    count_unic = 0
+    for k, v in count_symbol.items():
+        if v == 1:
+            count_unic += 1
+    return count_unic
+
+print(f'Количество уникальных символов в строке: {count_unique_characters(message)}')
+
+
+
+# Решение 2.1 - решение 1 с генератором списка (list comprehension)
+
+from collections import Counter
+
+message = "Today is a beautiful day! The sun is shining and the birds are singing."
+
+def count_unique_characters(message:str) -> int:
+    count_symbol = Counter(message.lower())
+    count_unic = len([value for key, value in count_symbol.items() if value == 1])
+    return count_unic
+
+print(f'Количество уникальных символов в строке: {count_unique_characters(message)}')
