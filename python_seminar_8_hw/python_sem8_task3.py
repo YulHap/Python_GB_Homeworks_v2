00c3a907db77ef52f35b3432a1fa26dadeb3d07d
YuliYa <yuh0121@mail.ru>
2024-09-16 02:20:26 +0700

    Add sem8:task3

--- python_seminar_8_hw/python_sem8_task3.py
+++ python_seminar_8_hw/python_sem8_task3.py
@@ -22,8 +22,30 @@
 # 1) V. Petrov 98
 # 2) P. Sergeev 92
 
+
+
 with open("python_seminar_8_hw/file_for_hw8/first_tour.txt", "r", encoding="utf-8") as ft:
-    txt = ft.readlines()
-    k = int(txt[0])
+    txt = list(ft.readlines()) # ['80\n', 'Ivanov Serg 80\n', 'Sergeev Petr 92\n', 'Petrov Vasiliy 98\n', 'Vasiliev Maxim 78']
+    k = int(txt[0]) # 80
+    dict_player = dict()
+    for i in range(1, len(txt)):
+        data = txt[i].split()
+        key = str(int(data[2]))
+        dict_player[key] = f'{data[1][0]}. {data[0]}'
+
+# print(dict_player) -> {'80': 'S. Ivanov', '92': 'P. Sergeev', '98': 'V. Petrov', '78': 'M. Vasiliev'}
+
+count = 0
+second_tour = {}
+for key, value in dict_player.items():
+    if int(key) > 80:
+        second_tour[key] = dict_player[key]
+        count += 1
+sorted_sec_tour = sorted(second_tour.items(), reverse=True)
 
 
+
+with open("python_seminar_8_hw/file_for_hw8/second_tour.txt", "w", encoding="utf-8") as st:
+    print(count, file = st)
+    for i, value in enumerate(sorted_sec_tour, start = 1):
+        print(i, ') ', *value, sep ='', file = st)
